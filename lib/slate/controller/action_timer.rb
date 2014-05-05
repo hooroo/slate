fa4e5b51563a1e9e5c4145a95701546d2d923b8d
Rob Monie <robmonie@gmail.com>
2014-05-05 17:34:17 +1000

    fix way metrics is called

--- lib/slate/controller/action_timer.rb
+++ lib/slate/controller/action_timer.rb
@@ -1,13 +1,11 @@
-require 'slate/metrics/timed_result'
+# require 'slate/metrics/timed_result'
 
 module  Slate
   module Controller
     module ActionTimer
 
-      include Metrics
-
       def time_action
-        time('controller_request', { controller_name: controller_name, action_name: action_name, url: request.url } ) do
+        Metrics.time('controller_request', { controller_name: controller_name, action_name: action_name, url: request.url } ) do
           yield
         end
       end
