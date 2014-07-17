dc21623df749a367ec49648d5706c0cc2dcdb0fe
Michal Pisanko <michal@hooroo.com>
2014-07-17 16:56:22 +1000

    rename parameter

--- lib/slate/request_id_holder.rb
+++ lib/slate/request_id_holder.rb
@@ -2,13 +2,13 @@
 module Slate
   module RequestIdHolder
 
-    def self.request_id=(request_id)
-      request_id ||= SecureRandom.uuid
-      Thread.current[:request_id] = request_id
+    def self.request_id=(new_request_id)
+      Thread.current[:request_id] = new_request_id || SecureRandom.uuid
     end
 
     def self.request_id
       Thread.current[:request_id]
     end
+
   end
 end
