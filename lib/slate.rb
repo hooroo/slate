b1b2687e70bf83106fe6859b17aeb55f9358badb
Rob Monie <robmonie@gmail.com>
2014-10-29 15:21:03 +1100

    fix dependency issue

--- lib/slate.rb
+++ lib/slate.rb
@@ -3,6 +3,7 @@ require "slate/version"
 require "slate/logger"
 require "slate/metrics"
 require "slate/controller/request_id_manager"
+require "slate/controller/correlation_id_manager"
 require "slate/controller/action_timer"
 require "slate/future_factory"
 require "slate/railtie" if defined?(Rails)
